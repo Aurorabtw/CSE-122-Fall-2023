d9a35e8bfa38253ee341c1662239c699ed02ce5e
Tasneem Rahman <56597339+Aurorabtw@users.noreply.github.com>
2023-07-30 15:05:23 +0600

    r

---
+++ LAB-01/LAB_demo1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+cout << "Welcome to the wonderful world of C++!!!\n";
+cout << "Lab1 \t";
+cout<<"\n END\t PROGRAM\t BYE";
+cout<<"Welcome in yarmouk univerity\rHellooo";
+}
---
+++ LAB-01/Lab Problem 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+
+    cout<< "Hello in my university\n\n";
+    cout<< "I'm a student of IT faculty\n\n";
+    cout<< "Welcome\n\n";
+    cout<< "Welcome\n\n";
+    return 0;
+}
