18192f65cd824c534e1264e06c1e5f294eede515
Tasneem Rahman <56597339+Aurorabtw@users.noreply.github.com>
2023-08-06 15:08:24 +0600

    Update LAB_demo1.cpp

--- LAB-01/LAB_demo1.cpp
+++ LAB-01/LAB_demo1.cpp
@@ -7,3 +7,28 @@ cout << "Lab1 \t";
 cout<<"\n END\t PROGRAM\t BYE";
 cout<<"Welcome in yarmouk univerity\rHellooo";
 }
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string abbreviateWord(const string& word) {
+    int length = word.length();
+    if (length > 10) {
+        return word[0] + to_string(length - 2) + word[length - 1];
+    }
+    return word;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        string word;
+        cin >> word;
+        cout << abbreviateWord(word) << endl;
+    }
+
+    return 0;
+}
